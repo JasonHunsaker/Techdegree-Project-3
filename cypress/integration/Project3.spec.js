68fd0a3edd5791e2b9e80d356b3837c391e9a7dc
Jason Hunsaker <jasonmhunsaker@gmail.com>
2022-02-24 21:21:25 -0500

    Update Project3.spec.js

--- cypress/integration/Project3.spec.js
+++ cypress/integration/Project3.spec.js
@@ -13,8 +13,27 @@ describe('project3 actions', () => {
         cy.get('#email').type('Jason@test.com')
     })
     
-    it('should select a t-shirt size', () => {
-        
+    it('should select a t-shirt size and design', () => {
+        cy.get('#size').select('large')
+        cy.get('#design').select('js puns')
+        cy.get('#color').select('Dark Slate Grey (JS Puns shirt only)')
+    })
+
+    it('should select a job role', () => {
+        cy.get('#title').select('Designer')
+    })
+
+    it('should register for activities', () => {
+        cy.get('#activities-box > :nth-child(1)').click()
+        cy.get('#activities-box > :nth-child(4)').click()
+        cy.get('#activities-box > :nth-child(7)').click()
+    })
+
+    
+
+    it('should make sure the total is correct', () => {
+     
+
     })
 })
 
