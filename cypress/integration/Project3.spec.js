e223b94f4452c5f5370823bcf49ba92dd630fd13
Jason Hunsaker <jasonmhunsaker@gmail.com>
2022-02-24 21:06:04 -0500

    Update Project3.spec.js

--- cypress/integration/Project3.spec.js
+++ cypress/integration/Project3.spec.js
@@ -1,6 +1,22 @@
 /// <reference types="cypress" />
 
-it('should navigate to google trends to get top searches', () => {
-    cy.visit('http://localhost:52330/index.html')
+describe('project3 actions', () => {
+    it('should visit the webapp', () => {
+        cy.visit('http://localhost:52330/index.html')
+    })
+
+    it('should fill in a name', () => {
+        cy.get('#name').type('Jason')
+    })
+
+    it('enter an email address', () => {
+        cy.get('#email').type('Jason@test.com')
+    })
     
+    it('should select a t-shirt size', () => {
+        
+    })
 })
+
+
+
